70097bd987f5b912b097c5f6e04492c6079742ff
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2022-05-02 05:16:42 -0700

    Add kh2 FormLock

---
+++ kh2/FormLock.lua
@@ -0,0 +1,268 @@
+-- TODO: JP support, keep party members, ensure game is beatable as forms other than final
+local gl = 0x68
+local jp = 0x66
+local offset = 0x56454E
+local now = 0x0714DB8 - offset
+local game_version = 0x17D - offset
+
+local game_speed = 0x715230 - offset
+local current_form_addr = 0x9AA5D4 - offset
+local zero_action_code = 0x3D577B - offset
+local add_revert_code = 0x3F072D - offset
+local decrease_form_code = 0x3BE45C - offset
+local player_ptr_addr = 0x2AE7128 - offset
+
+local form_delay_timer = 0
+
+local normal_form = 0
+local valor_form = 1
+local wisdom_form = 2
+local limit_form = 3
+local master_form = 4
+local final_form = 5
+
+local target_form = final_form
+
+local drive_action_table = {
+    [valor_form] = 0x0006,
+    [wisdom_form] = 0x0007,
+    [limit_form] = 0x02A1,
+    [master_form] = 0x000B,
+    [final_form] = 0x000C,
+}
+
+local form_keyblade_table = {
+    [normal_form] = 0x9A9560+0x40,
+    [valor_form] = 0x9AA364+0x40,
+    [wisdom_form] = nil,
+    [limit_form] = nil,
+    [master_form] = 0x9AA40C+0x40,
+    [final_form] = nil,
+}
+
+function _OnInit()
+    local form_string = {
+        [valor_form] = 'Valor Form',
+        [wisdom_form] = 'Wisdom Form',
+        [limit_form] = 'Limit Form',
+        [master_form] = 'Master Form',
+        [final_form] = 'Final Form',
+    }
+
+    if ReadByte(game_version) ~= gl then
+        -- TODO: JP support
+        print('FormLock ' .. form_string[target_form] .. ' initialized: JP')
+    else
+        print('FormLock ' .. form_string[target_form] .. ' initialized: Global')
+    end
+end
+
+function _OnFrame()
+    local world = ReadByte(now+0x00)
+    local room = ReadByte(now+0x01)
+    local place = ReadShort(now+0x00)
+    local door = ReadShort(now+0x02)
+    local map = ReadShort(now+0x04)
+    local btl = ReadShort(now+0x06)
+    local evt = ReadShort(now+0x08)
+    local prevPlace = ReadShort(now+0x30)
+
+    function events(m, b, e)
+        return (map == m or not m) and (btl == b or not b) and (evt == e or not e)
+    end
+
+    -- Remove Revert button from command menu
+    WriteArray(add_revert_code, {0x90, 0x90, 0x90, 0x90, 0x90})
+
+    local current_form = ReadByte(current_form_addr)
+    if current_form == target_form then
+        local anim = GetPlayerAnimation()
+        if anim == 0 then
+            -- If T-stanced, switch to actual animation
+            SetPlayerAnimation(16, 2)
+        -- elseif anim == FindPlayerAnimation(52, 1, true) then
+        --     SetPlayerAnimation(0, 2)
+        end
+
+        -- Give infinite form gauge in target form
+        WriteArray(decrease_form_code, {0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90})
+    else
+        WriteArray(decrease_form_code, {0xF3, 0x0F, 0x11, 0x8B, 0xB4, 0x01, 0x00, 0x00})
+    end
+
+    -- Force player into drive form if in normal form and not on carpet or in Armored Xemnas II fight
+    if current_form == 0 and place ~= 0x0E07 and place ~= 0x0507 and place ~= 0x1712 then
+        if form_delay_timer > 0 then
+            form_delay_timer = form_delay_timer - ReadFloat(game_speed)
+        else
+            SetAction(drive_action_table[target_form])
+            WriteArray(zero_action_code, {0x90, 0x90, 0x90})
+        end
+    else
+        form_delay_timer = 30
+        WriteArray(zero_action_code, {0x66, 0x89, 0x01})
+    end
+
+    -- Give Final Form a weapon if it doesn't have one
+    if GetFormWeapon(target_form) == 0x0000 then
+        WriteShort(0x009AA444+0x40-offset, 0x0180) -- Set weapon to Struggle Sword
+    end
+
+    UnlockForm(target_form)
+
+    if place == 0x1804 then -- If in CoR puzzle room give aerial dodge max
+        WriteByte(0x2A20E48+0x40+2-offset, 0x04)
+    elseif place == 0x0F0A and events(0x3B, 0x3B, 0x3B) then -- If Groundshaker fight give infinite aerial dodge
+        WriteByte(0x2A20E48+0x40+2-offset, 0x05)
+    end
+
+    -- Pride Lands
+    if world == 0x0A then
+        -- Reset to lion form through loading zones
+        if ReadLong(player_ptr_addr) == 0 then
+            WriteByte(current_form_addr, 0)
+        end
+
+        -- Give Glide 4
+        WriteByte(0x2A20E48+0x40+3-offset, 0x04)
+    end
+
+    -- Change Roxas skateboards
+    WriteString(0x2A37BA0+0x40-offset, "F_TT010_SORA")
+    WriteString(0x2A37BC0+0x40-offset, "F_TT010_SORA.mset")
+end
+
+function ComputeCurrentGrowthLevel(form)
+    local growth_param = 0x2A20E68
+    local growth_levels = 0x5C7E40
+
+    local offset_table = {
+        [valor_form] = 0x00,
+        [wisdom_form] = 0x05,
+        [master_form] = 0x0A,
+        [final_form] = 0x0F,
+        [limit_form] = 0x14,
+    }
+
+    local level = 4
+    while level >= 0 do
+        local e = ReadByte(growth_levels + offset_table[form] + level - offset)
+        if ((ReadInt(growth_param + ((e >> 5) * 4) - offset) >> (e & 0x1F)) & 1) ~= 0 then
+            break
+        end
+        level = level - 1
+    end
+
+    return level + 1
+end
+
+function UnlockForm(form)
+    local form_bit = {
+        [valor_form] = 1 << 1,
+        [wisdom_form] = 1 << 2,
+        [master_form] = 1 << 6,
+        [final_form] = 1 << 4,
+    }
+
+    if form == limit_form then
+        WriteByte(0x9AA738+0x40-offset, ReadByte(0x9AA738+0x40-offset) | 0x80000)
+    else
+        WriteByte(0x9AA730+0x40-offset, ReadByte(0x9AA730+0x40-offset) | form_bit[form])
+    end
+end
+
+function GetFormWeapon(form)
+    local addr = form_keyblade_table[form]
+
+    if addr ~= nil then
+        return ReadShort(form_keyblade_table[form]-offset)
+    else
+        return nil
+    end
+end
+
+function SetFormWeapon(form, weapon)
+    local addr = form_keyblade_table[form]
+    if addr ~= nil then
+        WriteShort(form_keyblade_table[form]-offset, weapon)
+    end
+end
+
+function GetAction()
+    local ptr = ReadLong(0x2A0DD50 - offset)
+    if ptr == 0 then
+        return nil
+    else
+        return ReadShort(ptr + 0x8, action, true)
+    end
+end
+
+function SetAction(action)
+    local ptr = ReadLong(0x2A0DD50 - offset)
+    if ptr ~= 0 then
+        WriteShort(ptr + 0x8, action, true)
+    end
+end
+
+function GetPlayerAnimation()
+    local playerPtr = ReadLong(player_ptr_addr)
+    if playerPtr == 0 then return nil end
+
+    return ReadLong(playerPtr+0x170, true)
+end
+
+function FindPlayerAnimation(motionId, relativeSlot, onlyFirst)
+    local animationFallback = {
+        [0] = { 0, 1, 3, 2 },
+        [1] = { 1, 0, 2, 3 },
+        [2] = { 2, 3, 1, 0 },
+        [3] = { 3, 2, 0, 1 },
+    }
+
+    local offsetMask = 0x01FFFFFF
+
+    local playerPtr = ReadLong(player_ptr_addr)
+    if playerPtr == 0 then return end
+
+    local msetPtr = ReadLongA(playerPtr+0x158)
+    if msetPtr == 0 then return end
+
+    local entry
+    local subfileName
+    local offset
+    for _, slot in ipairs(animationFallback[relativeSlot]) do
+        entry = ((slot + motionId * 4) + 1) << 4
+        subfileName = ReadInt(msetPtr+entry+4, true)
+        offset = ReadInt(msetPtr+entry+8, true) & offsetMask
+
+        -- not DUMM
+        if subfileName ~= 0x4D4D5544 then
+            break
+        end
+    end
+
+    local msetUpper = msetPtr & (~offsetMask & 0xFFFFFFFFFFFFFFFF)
+    local barPtr = msetUpper | offset
+
+    local animPtr = msetUpper | (ReadInt(barPtr + 0x18, true) & offsetMask)
+    local animPtr2 = msetUpper | (ReadInt(barPtr + 0x28, true) & offsetMask)
+
+    if onlyFirst == true then
+        return animPtr
+    else
+        return animPtr, animPtr2
+    end
+end
+
+function SetPlayerAnimation(motionId, relativeSlot)
+    local animPtr, animPtr2 = FindPlayerAnimation(motionId, relativeSlot)
+
+    local playerPtr = ReadLong(player_ptr_addr)
+    if playerPtr == 0 then return end
+
+    local playerAnimPtr = playerPtr+0x170
+    local playerAnimPtr2 = playerPtr+0x178
+
+    WriteLong(playerAnimPtr, animPtr, true)
+    WriteLong(playerAnimPtr2, animPtr2, true)
+end
